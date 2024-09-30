046835a7b8074a883c96bfff058daa80de4a09ff
jef4j <jefardu7@gmail.com>
2024-09-29 21:58:34 -0500

    integracionFirebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyAWcp7BZZQiCGGVdoy15sxhsr_wSY4LzTE",
+    authDomain: "taller-2-ionic-85b80.firebaseapp.com",
+    projectId: "taller-2-ionic-85b80",
+    storageBucket: "taller-2-ionic-85b80.appspot.com",
+    messagingSenderId: "978334843944",
+    appId: "1:978334843944:web:d6d367a1fe027f39d8e11d"
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyAWcp7BZZQiCGGVdoy15sxhsr_wSY4LzTE",
+    authDomain: "taller-2-ionic-85b80.firebaseapp.com",
+    projectId: "taller-2-ionic-85b80",
+    storageBucket: "taller-2-ionic-85b80.appspot.com",
+    messagingSenderId: "978334843944",
+    appId: "1:978334843944:web:d6d367a1fe027f39d8e11d"
+
+  }
+    
+
 };
 
 /*
