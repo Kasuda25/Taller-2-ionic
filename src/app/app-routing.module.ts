ef2772624c2bd1e55aab6535d758003bb91a0afe
Oscar Caraballo <caraballooscardavd@gmail.com>
2024-10-01 12:35:06 -0500

    se crear pagina de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,16 @@ const routes: Routes = [
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+
+
 ];
 
 @NgModule({
