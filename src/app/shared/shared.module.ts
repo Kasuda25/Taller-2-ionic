79761a53f17f0a226b73f9ca57b18ef7ece9e1eb
Oscar Caraballo <caraballooscardavd@gmail.com>
2024-10-02 00:26:56 -0500

    shared component input

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './components/input/input.component';
 
-
+const component = [InputComponent]
 
 @NgModule({
-  declarations: [],
+  declarations: [ ... component],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[ ... component]
 })
 export class SharedModule { }
