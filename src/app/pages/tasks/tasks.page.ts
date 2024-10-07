f77e3815b2c9cdab4d5fe78709f3038603fdc6be
jef4j <jefardu7@gmail.com>
2024-10-06 22:33:21 -0500

    create task

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { LoadingService } from 'src/app/shared/controllers/loading/loading.service';
+import { ToastService } from 'src/app/shared/controllers/toastService/toast.service';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { DatabaseService } from 'src/app/shared/services/database/database.service';
 
 @Component({
   selector: 'app-tasks',
@@ -11,7 +17,8 @@ export class TasksPage implements OnInit {
   public description!: FormControl;
   public taskForm!: FormGroup;
 
-  constructor() {
+  constructor(private readonly authSrv: AuthService, private readonly dbSrv: DatabaseService, private readonly loadingSrv: LoadingService,
+    private readonly nvctrl: NavController, private readonly toastSrv: ToastService) {
     this.initForm()
 
    }
@@ -20,8 +27,23 @@ export class TasksPage implements OnInit {
 
   }
 
-  public doCreate(){
-
+  public async doCreate(){
+    try {
+      await this.loadingSrv.show();
+      const { title, description } = this.taskForm.value;
+      const taskData = {
+        title: title,
+        description: description
+      };
+      await this.dbSrv.createTask(taskData);
+      await this.loadingSrv.dismiss();
+      this.nvctrl.navigateForward("home");
+      await this.toastSrv.presentToast("Se ha guardado la tarea", "success", "checkmark");
+    } catch (error) {
+      await this.loadingSrv.dismiss();
+      await this.toastSrv.presentToast("Error al guardar la tarea", "danger", "close");
+      console.error(error);
+    }
   }
 
 
--- src/app/shared/services/database/database.service.ts
+++ src/app/shared/services/database/database.service.ts
@@ -4,8 +4,6 @@ import { from, Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { switchMap } from 'rxjs/operators';
 
-
-
 export interface Task {
   id?: string;
   title: string;
@@ -15,65 +13,16 @@ export interface Task {
   done: boolean;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class DatabaseService {
-
   private TasksTable = 'tasks';
 
-
-  constructor(public fireStore: AngularFirestore, 
+  constructor(
+    public fireStore: AngularFirestore,
     public authsrv: AuthService
-  ) { }
-
-  public createTaskTable(task: Task): Promise<void> {
-    return this.authsrv.isAuth().then((isAuth) => {
-      if (isAuth) {
-        const id = this.fireStore.createId();  // Generar un ID único
-        return this.fireStore.collection(this.TasksTable).doc(id).set({ ...task, id });
-      } else {
-        return Promise.reject('Usuario no autenticado');
-      }
-    });
-  }
-
-  public GetTaskTable(userId: string): Observable<Task[]> {
-    return from(this.authsrv.isAuth()).pipe(
-      switchMap((isAuth) => {
-        if (isAuth) {
-          return this.fireStore.collection<Task>(this.TasksTable, ref =>
-            ref.where('userId', '==', userId)).valueChanges();
-        } else {
-          throw new Error('Usuario no autenticado');
-        }
-      })
-    );
-  }
-
-  public updateTaskTable(taskId: string, task: Task): Promise<void> {
-    return this.authsrv.isAuth().then((isAuth) => {
-      if (isAuth) {
-        return this.fireStore.collection(this.TasksTable).doc(taskId).update(task);
-      } else {
-        return Promise.reject('Usuario no autenticado');
-      }
-    });
-  }
-
-  public deleteTaskTable(taskId: string): Promise<void> {
-    return this.authsrv.isAuth().then((isAuth) => {
-      if (isAuth) {
-        return this.fireStore.collection(this.TasksTable).doc(taskId).delete();
-      } else {
-        return Promise.reject('Usuario no autenticado');
-      }
-    });
-  }
-
+  ) {}
 
   public addUser(uid: string, userData: any) {
     return this.fireStore.collection('users').doc(uid).set(userData);
@@ -91,4 +40,55 @@ export class DatabaseService {
       throw error;
     }
   }
+
+  public async createTask(task: any): Promise<void> {
+    const isAuth = await this.authsrv.isAuth();
+    if (isAuth) {
+      const id = this.fireStore.createId(); // Generar un ID único
+      return this.fireStore
+        .collection(this.TasksTable)
+        .doc(id)
+        .set({ ...task, id });
+    } else {
+      return Promise.reject('Usuario no autenticado');
+    }
+  }
+
+  public getTask(userId: string): Observable<Task[]> {
+    return from(this.authsrv.isAuth()).pipe(
+      switchMap((isAuth) => {
+        if (isAuth) {
+          return this.fireStore
+            .collection<Task>(this.TasksTable, (ref) =>
+              ref.where('userId', '==', userId)
+            )
+            .valueChanges();
+        } else {
+          throw new Error('Usuario no autenticado');
+        }
+      })
+    );
+  }
+
+  public async updateTask(taskId: string, task: Task): Promise<void> {
+    const isAuth = await this.authsrv.isAuth();
+    if (isAuth) {
+      return this.fireStore
+        .collection(this.TasksTable)
+        .doc(taskId)
+        .update(task);
+    } else {
+      return Promise.reject('Usuario no autenticado');
+    }
+  }
+
+  public async deleteTask(taskId: string): Promise<void> {
+    const isAuth = await this.authsrv.isAuth();
+    if (isAuth) {
+      return this.fireStore.collection(this.TasksTable).doc(taskId).delete();
+    } else {
+      return Promise.reject('Usuario no autenticado');
+    }
+  }
+
 }
